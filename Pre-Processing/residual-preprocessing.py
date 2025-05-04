872add6dc32cf8da0f5475196e4e6d892e00aa2f
wolfdemits <57060741+wolfdemits@users.noreply.github.com>
2025-05-04 15:43:14 +0200

    Add files via upload

--- Pre-Processing/residual-preprocessing.py
+++ Pre-Processing/residual-preprocessing.py
@@ -21,33 +21,57 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 slicemanager = Slice_manager()
-slicemanager.ENABLE_CROPPING = False
+slicemanager.DIFFUSION = True
 
-number_of_mice = 7
-print(f'{bcolors.OKBLUE}Total amount of mice: {number_of_mice}{bcolors.ENDC}')
+subjList = ['Mouse01', 'Mouse02', 'Mouse03', 'Mouse04', 'Mouse05'] # CHANGE
+print(f'{bcolors.OKBLUE}Total amount of mice: {len(subjList)}{bcolors.ENDC}')
 
 # open zarr store
 root = zarr.open_group(str(RESIDUAL_PATH), mode='a')
 
-for i in range(number_of_mice*6):
-    print(f'{bcolors.OKCYAN}Processing mouse: {slicemanager.mouse_list[round(np.floor(i/6))]}, loc: {slicemanager.current_loc}, plane: {slicemanager.current_plane}{bcolors.ENDC}', flush=True)
+for subj in subjList:
+    id = slicemanager.mouse_list.index(subj)
+    slicemanager.set_mouse_id(id)
+    for i in range(6):
+        print(f'{bcolors.OKCYAN}Processing mouse: {subj}, loc: {slicemanager.current_loc}, plane: {slicemanager.current_plane}{bcolors.ENDC}', flush=True)
 
-    for s in range(len(slicemanager.current_slice_list)):
-        normalise = lambda x: (x - np.mean(x)) / np.std(x)
-        
-        lr = slicemanager.load_scan(resolution='LOW RES',i=s)
-        hr = slicemanager.load_scan(resolution='HIGH RES',i=s)
-        lr_norm = normalise(lr)
-        hr_norm = normalise(hr)
-        lr_int = scipy.ndimage.zoom(lr_norm, zoom=2, order=0)
-        R = lr_int - hr_norm
+        for s in range(len(slicemanager.current_slice_list)):
+            normalise = lambda x: (x - np.mean(x)) / np.std(x)
+            
+            # always normalise when loading!!
+            lr = normalise(slicemanager.load_scan(resolution='LOW RES',i=s))
+            hr = normalise(slicemanager.load_scan(resolution='HIGH RES',i=s))
 
-        name = "Residual_" + slicemanager.get_slice_ID()[:-2] + f"{s + 1:02}"
+            lr_int = scipy.ndimage.zoom(lr, zoom=2, order=0)
+            R = lr_int - hr
 
-        zarr_arr = root.create_array(name, shape=R.shape ,chunks=R.shape, dtype='float', overwrite=True)
-        zarr_arr[:] = R
+            name = "Residual_" + slicemanager.get_slice_ID()[:-2] + f"{s + 1:02}"
 
-    # go to next scan
-    slicemanager.next_scan()
+            zarr_arr = root.create_array(name, shape=R.shape ,chunks=R.shape, dtype='float', overwrite=True)
+            zarr_arr[:] = R
+
+        # go to next scan
+        slicemanager.next_scan()
+
+# normalize variance
+total_arr = np.zeros((len(root.members()), R.shape[0], R.shape[1]))
+for i, residual in enumerate(root.keys()):
+    total_arr[i] = root[residual][:]
+
+sn = np.mean(np.std(total_arr, axis=0))
+
+# clear some memory
+del total_arr
+
+for residual in root.keys():
+    root[residual][:] = root[residual][:] / sn
 
 print(f'{bcolors.OKCYAN}Total amount of residuals: {len(root.members())}{bcolors.ENDC}')
+
+# test
+total_arr = np.zeros((len(root.members()), R.shape[0], R.shape[1]))
+for i, residual in enumerate(root.keys()):
+    total_arr[i] = root[residual][:]
+
+sn = np.mean(np.std(total_arr, axis=0))
+print(f'{bcolors.OKGREEN}CHECK: Population variance: {sn}{bcolors.ENDC}')
